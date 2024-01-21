fdd09dc9766cac8d9ffb05a78f06e6393e58a1cd
PerlaGCastillo <contactoperlagodinez@gmail.com>
2024-01-21 14:21:43 -0600

    db url var

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ from psycopg2 import connect, extras
 load_dotenv()
 app = Flask(__name__)
 
-host = environ.get('DB_HOST')
-port = environ.get('DB_PORT')
-dbname = environ.get('DB_NAME')
-user = environ.get('DB_USER')
-password = environ.get('DB_PASSWORD')
-
+url = environ.get('DB_URL')
 
 def get_connection():
-    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
+    conn = connect(url=url)
     return conn
 
 
